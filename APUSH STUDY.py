f83af68a29a87cb91bdb402c0f3099b839d414c3
romcommulus <49224306+romcommulus@users.noreply.github.com>
2019-04-09 12:47:01 -0700

    Add files via upload

---
+++ APUSH STUDY.py
@@ -0,0 +1,241 @@
+import time
+print("Welcome to The APUSH Trivia Game!")
+
+
+
+
+def apush():   
+    while True:
+        time.sleep(.5)
+        print("1.A “vertically integrated” system of production is one in which?: ")
+        time.sleep(.1)
+        print("    a.Employees of different ethnic origins work together on the assembly line.")
+        time.sleep(.1)
+        print("    b. A single company controls the entire industrial process from source of raw materials to the final market")
+        time.sleep(.1)
+        print("    c. Management and labor share equality in profits through an elaborate sharing system") 
+        time.sleep(.1)
+        print("    d. All the employees belong to one big union organized by industry not by craft")
+        ans = input("Which of the following is true?: ")
+        if ans == "b":
+            print("Correct Answer")
+            break
+        elif ans == "B":
+            print("Correct Answer")
+            break
+        else:
+            print("Wrong Try Again")
+apush()
+            
+def apush2():   
+    while True:
+        time.sleep(.5)
+        print("2.Which of the following groups were excluded from immigration to the United States by laws passed in the 1880s and 1890s?: ")
+        time.sleep(.1)
+        print("    a.Japanese")
+        time.sleep(.1)
+        print("    b.Irish")
+        time.sleep(.1)
+        print("    c.Chinese")
+        time.sleep(.1)
+        print("    d.Convicts, paupers, and mental incompetents")   
+        ans = input("Which of the following is true?: ")
+        if ans == "d":
+            print("Correct Answer")
+            break
+        elif ans == "D":
+            print("Correct Answer")
+            break
+        else:
+            print("Wrong Try Again")
+apush2()
+
+def apush3():   
+    while True:
+        time.sleep(.5)
+        print("3.American urban population growth from the 1890s resulted mainly from: ")
+        time.sleep(.1)
+        print("    a.Low rate of infant mortality")
+        time.sleep(.1)
+        print("    b.Increase instance of premarital sex")
+        time.sleep(.1)
+        print("    c.Large influx of new immigrants")
+        time.sleep(.1)
+        print("    d.Increased fertility rate")   
+        ans = input("Which of the following is true?: ")
+        if ans == "c":
+            print("Correct Answer")
+            break
+        elif ans == "C":
+            print("Correct Answer")
+            break
+        else:
+            print("Wrong Try Again")
+apush3()
+
+def apush4():   
+    while True:
+        time.sleep(.5)
+        print("4.Due to the rapid growth of industrialization in the North people moved out of rural towns to?: ")
+        time.sleep(.1)
+        print("    a.Urbanized cities")
+        time.sleep(.1)
+        print("    b.Rural farms")
+        time.sleep(.1)
+        print("    c.El Rancho")
+        time.sleep(.1)
+        print("    d.Suburbs cities")   
+        ans = input("Which of the following is true?: ")
+        if ans == "a":
+            print("Correct Answer")
+            break
+        elif ans == "A":
+            print("Correct Answer")
+            break
+        else:
+            print("Wrong Try Again")
+apush4()
+
+def apush5():
+    while True:
+        time.sleep(.5)
+        print("5.What was the main purpose of the Boston Tea Party in 1773?: ")
+        time.sleep(.1)
+        print("    a.The act of pouring Tea into the harbour was a yearly tradition locals performed")
+        time.sleep(.1)
+        print("    b.An act of political protest due to the increase of taxes")
+        time.sleep(.1)
+        print("    c.All of the tea has expired and they dumped it into the harbour")
+        time.sleep(.1)
+        print("    d.A ship sunk, dumping enormous amounts of tea into the harbour")
+        ans = input("Which of the following is true?: ")
+        if ans == "b":
+            print("Correct Answer")
+            break
+        elif ans == "B":
+            print("Correct Answer")
+            break
+        else:
+            print("Wrong Try Again")
+            
+apush5()
+
+def apush6():
+    while True:
+        time.sleep(.5)
+        print("6.Who was the president after Theodore Roosevelt?: ")
+        time.sleep(.1)
+        print("    a.Abraham Lincoln")
+        time.sleep(.1)
+        print("    b.Grover Cleveland")
+        time.sleep(.1)
+        print("    c.William McKinley")
+        time.sleep(.1)
+        print("    d.William Howard Taft")
+        ans = input("Which of the following is true?: ")
+        if ans == "d":
+            print("Correct Answer")
+            break
+        elif ans == "D":
+            print("Correct Answer")
+            break
+        else:
+            print("Wrong Try Again")
+apush6()
+
+def apush7():
+    while True:
+        time.sleep(.5)
+        print("7.Who said “I predict future happiness for Americans, if they can prevent the government from wasting the labors of the people under the pretense of taking care of them.”")
+        time.sleep(.1)
+        print("    a.Harry S. Truman")
+        time.sleep(.1)
+        print("    b.Thomas Jefferson")
+        time.sleep(.1)
+        print("    c.Warren G.Harding")
+        time.sleep(.1)
+        print("    d.James A.Garfield")
+        ans = input("Which of the following is true?: ")
+        if ans == "b":
+            print("Correct Answer")
+            break
+        elif ans == "B":
+            print("Correct Answer")
+            break
+        else:
+            print("Wrong Try Again")
+apush7()
+
+def apush8():
+    while True:
+        time.sleep(.5)
+        print("8.Which woman was the Founder of Settlement House Movement and was the first woman to earn a Nobel Peace Prize in 1931.")
+        time.sleep(.1)
+        print("    a.Abigail Adams")
+        time.sleep(.1)
+        print("    b.Jane Addams")
+        time.sleep(.1)
+        print("    c.Marian Anderson")
+        time.sleep(.1)
+        print("    d.Helen Keller")
+        ans = input("Which of the following is true?: ")
+        if ans == "b":
+            print("Correct Answer")
+            break
+        elif ans == "B":
+            print("Correct Answer")
+            break
+        else:
+            print("Wrong Try Again")
+apush8()
+
+def apush9():
+    while True:
+        time.sleep(.5)
+        print("9. What was the goal of the North Atlantic Treaty Organization (NATO) when it was formed in 1949?")
+        time.sleep(.1)
+        print("    a.Create an intergovernmental military alliance and deter Soviet aggression")
+        time.sleep(.1)
+        print("    b.Protect North Atlantic Trade")
+        time.sleep(.1)
+        print("    c.Stop imperialism and colonization")
+        time.sleep(.1)
+        print("    d.Prevent the spread of communism")
+        ans = input("Which of the following is true?: ")
+        if ans == "a":
+            print("Correct Answer")
+            break
+        elif ans == "A":
+            print("Correct Answer")
+            break
+        else:
+            print("Wrong Try Again")
+apush9()
+
+def apush10():
+    time.sleep(.5)
+    while True:
+        print("10. What was President Harry S. Truman known best for?")
+        time.sleep(.1)
+        print("    a.Under Presidency of Truman, the US engages in an internationalist foreign policy, a departure from its policy of isolationism.")
+        time.sleep(.1)
+        print("    b.Because he asked congress for $400 million in military and economic assistance for Turkey and Greece.")
+        time.sleep(.1)
+        print("    c.In his first months in office he dropped the atomic bomb on Japan, ending World War II.")
+        time.sleep(.1)
+        print("    d.He instituted the Marshall Plan, which gave billions of dollars in aid to help stimulate economic recovery in European nations.")
+        ans = input("Which of the following is true?: ")
+        if ans == "c":
+            print("Correct Answer")
+            break
+        elif ans == "D":
+            print("Correct Answer")
+            break
+        else:
+            print("Wrong Try Again")
+apush10()
+    
+
+
+
+            
